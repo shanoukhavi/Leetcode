7042b03cc9b3598591d83494a358474a4a3af025
shanoukhavi <168278161+shanoukhavi@users.noreply.github.com>
2024-07-12 17:07:36 +0530

    Time: 7 ms (63.35%), Space: 34.9 MB (22.86%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -6,17 +6,7 @@ public:
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         int ans=nums2[0]-nums1[0];
+
         return ans;
-     //    int ans=0;
-     // int i=0;
-     //    int j=0;
-     //    while(i<nums1.size() && j<nums2.size())
-     //    {
-     //        ans=nums2[j]-nums2[i];
-     //        i++;
-     //        j++;
-     //    }
-     //    return ans;
-        
     }
 };
